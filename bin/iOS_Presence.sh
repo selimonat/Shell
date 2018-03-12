2dcb895a67dccfebbc06b80682c642904b23ddc1
selimonat <onatselim@gmail.com>
2018-03-12 22:59:55 +0100

    first import of home device presence detection

---
+++ bin/iOS_Presence.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ #Enter your ip of your device here
+declare -a DEVICES=("192.168.2.1" "192.168.2.2" "192.168.2.3" "192.168.2.4" "192.168.2.5" "192.168.2.6")
+declare -a OUTPUT=()
+for i in ${DEVICES[@]}; do
+    # Change dev and eth0 if needed
+    ip neigh flush dev eth0 $i
+    hping3 -2 -c 10 -p 5353 -i u1 $i -q >/dev/null 2>&1
+    sleep 1
+    # Only arp specific device, grep for a mac-address
+    status=`arp -an $i | awk '{print $4}' | grep "..:..:..:..:..:.."`
+    #statusMessage="OFF"
+    #A mac will be 17 characters including the ":"
+    if [ ${#status} -eq 17 ]; then
+        #echo "Phone $i is detected!"
+        #statusMessage="ON"
+        OUTPUT+=('1')
+    else
+        #echo "Phone $i is not present"
+        #statusMessage="OFF"
+        OUTPUT+=('0')
+    fi
+done
+echo ${OUTPUT[@]}
