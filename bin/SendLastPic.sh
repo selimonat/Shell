d4dfe4a18035eedf144ffd752cec0f8dd5c70287
selimonat <onatselim@gmail.com>
2018-03-19 21:27:16 +0100

    added path as first argument

--- bin/SendLastPic.sh
+++ bin/SendLastPic.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+#Argument must be a valid email address.
 
 
-last_file=`find ~/ -maxdepth 1 -printf "%T+ %p\n" | grep JPG | head -n 1 | rev | cut -d" " -f 1 | rev` 
+last_file=`find $1 -maxdepth 1 -printf "%T+ %p\n" | grep JPG | head -n 1 | rev | cut -d" " -f 1 | rev` 
 date_now=`date`
 mail_text="This is a visual taken from your GoPro camera at ${date_now}."
-(echo ${mail_text}; uuencode ${last_file} visual.jpg) | mail -s "Visual from your home" onatselim@gmail.com
+echo Sending email to $2
+(echo ${mail_text}; uuencode ${last_file} visual.jpg) | mail -s "Visual from your home" $2
